d001ce469302490b8a39509a470e351966ef0671
Aniket Kumar <aniket24kr@gmail.com>
2023-12-28 10:15:21 +0000

    Add error handling

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { type NextFunction, type Request, type Response } from 'express';
+import { isHttpError, InternalServerError, type HttpError } from 'http-errors';
 import config from './config';
 import morgan from 'morgan';
 
@@ -11,6 +12,32 @@ app.use(morgan('dev'));
 // routers
 import routers from './routers';
 app.use('/api/v1', routers);
+
+// error handling
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    function makeInternalError(err: Error) {
+        const httpError = new InternalServerError('Something went wrong!');
+        httpError.cause = err;
+        return httpError;
+    }
+
+    function debugInfo(httpError: HttpError) {
+        if (config.isDebug) {
+            const { cause, stack } = httpError;
+            return { cause, stack };
+        }
+        return {};
+    }
+    const httpError = isHttpError(err) ? err : makeInternalError(err);
+
+    res.status(httpError.status).json({
+        url: req.originalUrl,
+        status: httpError.status,
+        name: httpError.name,
+        message: httpError.message,
+        ...debugInfo(httpError),
+    });
+});
 
 // start listening
 const { port, host } = config;
